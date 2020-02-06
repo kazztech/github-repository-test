df35a549f9819c56667ff4397ad4a7422d0d075d
kazz <kazzzz1001@gmail.com>
2020-02-06 15:21:10 +0900

    update

---
+++ index.js
@@ -0,0 +1,23 @@
+"use strict";
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "atoms", {
+  enumerable: true,
+  get: function get() {
+    return _atoms["default"];
+  }
+});
+Object.defineProperty(exports, "molecules", {
+  enumerable: true,
+  get: function get() {
+    return _molecules["default"];
+  }
+});
+
+var _atoms = _interopRequireDefault(require("./atoms"));
+
+var _molecules = _interopRequireDefault(require("./molecules"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
