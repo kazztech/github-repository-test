8d4cd00361e013ef5b149128ee4f98aa9b223f55
kazz <kazzzz1001@gmail.com>
2020-02-12 11:26:40 +0900

    update

--- src/index.js
+++ src/index.js
@@ -1,2 +1,2 @@
-export { default as atoms } from "./atoms";
-export { default as molecules } from "./molecules";
+// export { default as atoms } from "./atoms";
+// export { default as molecules } from "./molecules";
