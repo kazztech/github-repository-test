81a8704dd41853486a3e8317d9f4cded2c26b072
kazz <kazzzz1001@gmail.com>
2020-02-12 12:47:59 +0900

    update

---
+++ .storybook/main.js
@@ -0,0 +1,4 @@
+module.exports = {
+  stories: ["../src/atoms/**/stories.js"],
+  addons: ["@storybook/addon-actions", "@storybook/addon-links"]
+};
--- src/atoms/Button/index.js
+++ src/atoms/Button/index.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 function Button(props) {
-  return <button>{props.children}</button>;
+  return (
+    <button
+      style={{
+        padding: props.size === "large" ? "8px 4px" : "4px"
+      }}
+    >
+      {props.children}
+    </button>
+  );
 }
 
 export default Button;
